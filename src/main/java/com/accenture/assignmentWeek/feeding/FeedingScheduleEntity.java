624ab7669a7eef75704c79df384cdd15850c126d
lptrk <lktpriv@gmail.com>
2023-10-09 12:20:18 +0200

    added AllArgsConstructor, NoArgsConstructor, Getters, Setters, equals and Hashcode to all Entities

--- src/main/java/com/accenture/assignmentWeek/feeding/FeedingScheduleEntity.java
+++ src/main/java/com/accenture/assignmentWeek/feeding/FeedingScheduleEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class FeedingScheduleEntity {
@@ -14,6 +15,71 @@ public class FeedingScheduleEntity {
     private Long id;
     private LocalTime feedingTime;
     private int numberOfFeedings;
+
+    @Override
+    public String toString() {
+        return "FeedingScheduleEntity{" +
+                "id=" + id +
+                ", feedingTime=" + feedingTime +
+                ", numberOfFeedings=" + numberOfFeedings +
+                ", horse=" + horse +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FeedingScheduleEntity that = (FeedingScheduleEntity) o;
+        return numberOfFeedings == that.numberOfFeedings && Objects.equals(id, that.id) && Objects.equals(feedingTime, that.feedingTime) && Objects.equals(horse, that.horse);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, feedingTime, numberOfFeedings, horse);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalTime getFeedingTime() {
+        return feedingTime;
+    }
+
+    public void setFeedingTime(LocalTime feedingTime) {
+        this.feedingTime = feedingTime;
+    }
+
+    public int getNumberOfFeedings() {
+        return numberOfFeedings;
+    }
+
+    public void setNumberOfFeedings(int numberOfFeedings) {
+        this.numberOfFeedings = numberOfFeedings;
+    }
+
+    public HorseEntity getHorse() {
+        return horse;
+    }
+
+    public void setHorse(HorseEntity horse) {
+        this.horse = horse;
+    }
+
+    public FeedingScheduleEntity() {
+    }
+
+    public FeedingScheduleEntity(Long id, LocalTime feedingTime, int numberOfFeedings, HorseEntity horse) {
+        this.id = id;
+        this.feedingTime = feedingTime;
+        this.numberOfFeedings = numberOfFeedings;
+        this.horse = horse;
+    }
 
     @ManyToOne
     @JoinColumn(name = "horse_id")
--- src/main/java/com/accenture/assignmentWeek/horse/HorseEntity.java
+++ src/main/java/com/accenture/assignmentWeek/horse/HorseEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class HorseEntity {
@@ -15,6 +16,122 @@ public class HorseEntity {
     private String name;
     private String nickName;
     private String breed;
+
+    @Override
+    public String toString() {
+        return "HorseEntity{" +
+                "rfid='" + rfid + '\'' +
+                ", name='" + name + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", breed='" + breed + '\'' +
+                ", owner='" + owner + '\'' +
+                ", stable=" + stable +
+                ", feedingSchedules=" + feedingSchedules +
+                ", lastFeedingTime=" + lastFeedingTime +
+                ", numberOfDailyFeedings=" + numberOfDailyFeedings +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HorseEntity that = (HorseEntity) o;
+        return Objects.equals(rfid, that.rfid) && Objects.equals(name, that.name) && Objects.equals(nickName, that.nickName) && Objects.equals(breed, that.breed) && Objects.equals(owner, that.owner) && Objects.equals(stable, that.stable) && Objects.equals(feedingSchedules, that.feedingSchedules) && Objects.equals(lastFeedingTime, that.lastFeedingTime) && Objects.equals(numberOfDailyFeedings, that.numberOfDailyFeedings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rfid, name, nickName, breed, owner, stable, feedingSchedules, lastFeedingTime, numberOfDailyFeedings);
+    }
+
+    public String getRfid() {
+        return rfid;
+    }
+
+    public void setRfid(String rfid) {
+        this.rfid = rfid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public StableEntity getStable() {
+        return stable;
+    }
+
+    public void setStable(StableEntity stable) {
+        this.stable = stable;
+    }
+
+    public List<FeedingScheduleEntity> getFeedingSchedules() {
+        return feedingSchedules;
+    }
+
+    public void setFeedingSchedules(List<FeedingScheduleEntity> feedingSchedules) {
+        this.feedingSchedules = feedingSchedules;
+    }
+
+    public LocalDateTime getLastFeedingTime() {
+        return lastFeedingTime;
+    }
+
+    public void setLastFeedingTime(LocalDateTime lastFeedingTime) {
+        this.lastFeedingTime = lastFeedingTime;
+    }
+
+    public Integer getNumberOfDailyFeedings() {
+        return numberOfDailyFeedings;
+    }
+
+    public void setNumberOfDailyFeedings(Integer numberOfDailyFeedings) {
+        this.numberOfDailyFeedings = numberOfDailyFeedings;
+    }
+
+    public HorseEntity() {
+    }
+
+    public HorseEntity(String rfid, String name, String nickName, String breed, String owner, StableEntity stable, List<FeedingScheduleEntity> feedingSchedules, LocalDateTime lastFeedingTime, Integer numberOfDailyFeedings) {
+        this.rfid = rfid;
+        this.name = name;
+        this.nickName = nickName;
+        this.breed = breed;
+        this.owner = owner;
+        this.stable = stable;
+        this.feedingSchedules = feedingSchedules;
+        this.lastFeedingTime = lastFeedingTime;
+        this.numberOfDailyFeedings = numberOfDailyFeedings;
+    }
+
     private String owner;
 
     @ManyToOne
