a586eb21801e0476c8ed74e987c3b3d205d7328f
lptrk <lktpriv@gmail.com>
2023-10-20 00:01:26 +0200

    set fixed Spring Security version (6.1.4)

--- api/src/main/java/dev/lptrk/horsefeeding/HorseFeedingApplication.java
+++ api/src/main/java/dev/lptrk/horsefeeding/HorseFeedingApplication.java
@@ -2,11 +2,13 @@ package dev.lptrk.horsefeeding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.SpringSecurityCoreVersion;
 
 @SpringBootApplication
 public class HorseFeedingApplication {
 
     public static void main(String[] args) {
+        System.out.println("Verwendete Spring Security-Version: " + SpringSecurityCoreVersion.getVersion());
         SpringApplication.run(HorseFeedingApplication.class, args);
     }
 
